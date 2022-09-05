6c0e29ef7f37a20ce8142eb48ffe6081104e27b8
hiteshzope21 <zope00hitesh@gmail.com>
2022-09-05 20:54:52 +0530

    Added CORS changes to get request from any PORT

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const cors = require("cors");
 const logger = require("morgan")
 
 
-
-let origin = 'http://localhost:8080';
-
-app.use(cors({
-    origin,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
